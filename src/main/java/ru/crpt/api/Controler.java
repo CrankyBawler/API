b7ada818a7bcf78c9614806d266ca9782baa1a52
Cranky Bawler <6666lenia6666@mail.ru>
2024-05-05 11:38:32 +0300

    1

---
+++ src/main/java/ru/crpt/api/Controler.java
@@ -0,0 +1,22 @@
+package ru.crpt.api;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Controler {
+private final CreatDocService creatDocService;
+
+    public Controler(CreatDocService creatDocService) {
+        this.creatDocService = creatDocService;
+    }
+
+    @PostMapping("/ismp.crpt.ru/api/v3/lk/documents/create")
+    public ResponseEntity<Document> addDocument(@PathVariable("id") Long id,
+                                                @RequestBody CreatDocService creatDocService) {
+        return ResponseEntity.ok(creatDocService.addDoc(id, creatDocService));
+    }
+}
